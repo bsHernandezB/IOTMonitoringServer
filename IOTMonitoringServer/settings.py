ea292ded046565477bce0a9144211e793d490c2a
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-09-18 01:09:24 -0500

    cambio ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.85.10.120"]
+ALLOWED_HOSTS = ["localhost", "54.235.19.186"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.87.133.134",  # Dirección IP de la base de datos
+        "HOST": "54.211.101.83",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.236.155.251"
+MQTT_HOST = "18.234.233.88"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
